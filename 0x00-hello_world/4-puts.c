7e06956e9807ed2f034bdda641dff5afa7369d6e
Akinbowale <akin.akinyemi81@gmail.com>
2022-06-30 17:29:31 -0700

    task2 3 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *
+ *Return: Always 0 (Suceess)
+ */
+int main(void)
+{
+  puts("\"Programming is like building a ultilingual puzzle");
+  return (0);
+}

157963ced225334ebedd8a1d71bb4c7b8a680da2
Akinbowale <akin.akinyemi81@gmail.com>
2022-07-04 17:52:48 -0700

    task 4 and 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+
+/**
+*main - rints single digit numbers in base 10
+*Return: Always 0 (Success)
+*/
+int main(void)
+{
+int x = 0;
+
+while (x < 10)
+{
+putchar(x);
+x = x + 1;
+}
+putchar(10)
+return (0);
+}

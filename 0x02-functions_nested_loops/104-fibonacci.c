6ebdd985d0a402201d30f3a7f678022f43fe3041
Akinbowale <akin.akinyemi81@gmail.com>
2022-07-06 04:21:37 -0700

    task 16

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -6,14 +6,14 @@
 */
 int main(void)
 {
-unsigned long int a, b, c, b_quotient, b_modulus, c_quotient, c_modulus
+unsigned long int a, b, c, b_quotient, b_modulus, c_quotient, c_modulus;
 b = 1;
 c = 2;
 
-printf("%lu", b)
+printf("%lu", b);
 for (a = 1; a < 94; a++)
 {
-printf(", %lu", b)
+printf(", %lu", b);
 c = c + b;
 b = c - b;
 }
@@ -23,7 +23,7 @@ c_quotient = c / 1000000000;
 c_modulus = c % 1000000000;
 for (a = 94; a < 99; a++)
 {
-printf(", %lu", c_quotient + (c_modulus / 1000000000);
+printf(", %lu", c_quotient + (c_modulus / 1000000000));
 printf("%lu", c_modulus % 1000000000);
 c_quotient = c_quotient + b_quotient;
 b_quotient = c_quotient - b_quotient;

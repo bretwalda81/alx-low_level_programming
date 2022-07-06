69894e8877e4de0d4b961fefc8819e571371cfad
Akinbowale <akin.akinyemi81@gmail.com>
2022-07-06 03:44:36 -0700

    advanced tasks

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+* main - computes and prints the sum of multiples
+* of 3 and 5
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+int i, sum;
+sum = 0;
+for (i = 0; i < 1024; i++)
+{
+if
+((i % 3) == 0 || (i % 5) == 0)
+{
+sum += i;
+}
+}
+printf("%d\n", sum);
+return (0);
+}

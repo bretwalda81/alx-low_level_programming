4c42c067f7d3c02616979275710bec1212ea39c5
Akinbowale <akin.akinyemi81@gmail.com>
2022-07-07 11:05:54 -0700

    task 8

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+* print_square - prints a square
+* @size: the parameter for the number of # to be printed
+*/
+void print_square(int size)
+{
+int a, b;
+
+for (a = 1; a <= size; a++)
+{
+if (n > 0)
+{
+for (b = 1; b <= size; b++)
+_putchar('#');
+}
+else
+{
+_putchar('\n');
+}
+_putchar('\n');
+}
+}

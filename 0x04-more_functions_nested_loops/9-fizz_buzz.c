bc28c3cc13aceff1cd9fd466cec92f63ad18e728
Akinbowale <akin.akinyemi81@gmail.com>
2022-07-07 12:05:46 -0700

    task 9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+* main - fizzbuzz
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+int i;
+for (i = 1; a <= 100; i++)
+{
+if ((a % 3 == 0) && (a % 5 == 0))
+printf("FizzBuzz ");
+else if (a % 3 == 0)
+printf("Fizz");
+else if (a % 5 == 0)
+printf("Buzz ");
+else
+printf("%d ", i);
+}
+return (0);
+}

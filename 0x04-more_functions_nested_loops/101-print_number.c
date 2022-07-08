37c772c70f635fd75329ae0e86d440813df2192b
Akinbowale <akin.akinyemi81@gmail.com>
2022-07-07 19:30:44 -0700

    task 12

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -6,14 +6,27 @@
 */
 void print_number(int n)
 {
+unsigned int b, c, n1, rnk, val;
+rnk = 1;
+c = 1;
+
 if (n < 0)
 {
-_putchar('-');
+_putchar('-')
 n = -n;
 }
-if (n / 10)
+n1 = n;
+while (n1 > 9)
 {
-print_number(n / 10);
+n1 = n1 / 10;
+c++;
+rnk = rnk * 10;
 }
-_putchar(n % 10 + '0');
+for (b = 0; b < c; b++)
+{
+val = n / rnk;
+n = n % rnk;
+rnk = rnk / 10;
+_putchar (val + '0');
+}
 }

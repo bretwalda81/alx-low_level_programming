649ca3680eb3ce4b334dfe18a30c53964cc2aafd
Akinbowale <akin.akinyemi81@gmail.com>
2022-07-25 15:57:53 -0700

    allocate dynamic spaces in memory

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -12,7 +12,7 @@ char *str_concat(char *s1, char *s2)
 {
 int i = 0;
 int j = 0;
-int k;
+ int k, l;
 char *s3 = NULL;
 
 if (s1 == NULL)
@@ -24,12 +24,16 @@ while (s1[i] != '\0')
 i++;
 while (s2[j] != '\0')
 j++;
-s3 = (char *)malloc(sizeof(char) * (i + j));
+s3 = (char *)malloc(sizeof(char) * (i + j) +1);
 if (s3 == NULL)
 return (NULL);
-for (k = 0; *s1; k++)
+else
+{
+for (k = 0; k < i; k++)
 s3[k] = s1[k];
-for (k = 0; *s2; k++)
-s3[k + i] = s2[k];
+for (l = 0; l < j; k++, l++)
+s3[k] = s2[l];
+s3[k] = '\0';
 return (s3);
+}
 }

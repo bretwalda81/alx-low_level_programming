b7e6f58a1ec068eef9eeb63ae0eab68c99b77a26
Akinbowale <akin.akinyemi81@gmail.com>
2022-07-28 16:44:23 -0700

    allocate more memory space

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -10,9 +10,9 @@
 */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-int i = 0;
-int j = 0;
-int k, l;
+unsigned int i = 0;
+unsigned int j = 0;
+unsigned int k, l;
 char *s3 = NULL;
 
 if (s1 == NULL)

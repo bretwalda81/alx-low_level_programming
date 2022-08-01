fe16377ac069882c75d5a2aa49f382b1519cf38f
Akinbowale <akin.akinyemi81@gmail.com>
2022-08-01 14:50:39 -0700

    create various macros

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+* main - prints tthe name of the file that it was compiled from
+* @void: nothing
+* Return: 0 if successful
+*/
+int main (void)
+{
+printf("%s\n", __FILE__);
+return (0);
+}

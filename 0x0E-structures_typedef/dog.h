bd978257be3e640940204eaa9ac1e810864a8985
Akinbowale <akin.akinyemi81@gmail.com>
2022-08-08 12:20:59 -0700

    prevent double inclusion on header file

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,4 +1,4 @@
-#ifndef D0G_H
+#ifndef DOG_H
 #define DOG_H
 
 /**
@@ -20,4 +20,5 @@ void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
+
 #endif
